42ebc5a168f7550000a7cc9d74715726d6cffa8d
Artur Gottmann <artur.gottmann@desy.de>
2024-10-11 17:23:22 +0200

    adding input dataset to config

--- main.py
+++ main.py
@@ -102,6 +102,7 @@ def prepare(args):
                     dataset_crab = copy.deepcopy((args["crab"]))
                     dataset_crab.General.requestName = "_".join([k,dt_period,dataset_type,dkey])
                     dataset_crab.JobType.psetName = str(args["conditions"][k][dt_period]["cmsrun"])
+                    dataset_crab.Data.inputDataset = dname
                     dataset_crab_path = crabconfigarea / dataset_crab.General.requestName
                     args["crabconfigs"][k][dt_period][dataset_type][dkey] = str(dataset_crab_path) + ".py"
                     # TODO: introduce a check for existence and checksum check
