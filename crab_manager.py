19c09a5697a10b1211556b95245cb3886b267981
Artur Gottmann <artur.gottmann@kit.edu>
2024-10-23 11:52:26 +0200

    split submission from remaining workflow (and run black formatting)

--- crab_manager.py
+++ crab_manager.py
@@ -1,10 +1,5 @@
 #! /usr/bin/env python3
 
-# TODO:
-# Add a check of for the input and output datasets about number of events.
-# This would allow to justify, that the production was completely successful.
-# Possible tool: DBS client bindings
-
 import os
 import glob
 import asyncio
@@ -20,23 +15,51 @@ import json
 import CRABClient
 from CRABAPI.RawCommand import crabCommand
 
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Script to manage a CMSSW production with crab",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
     # Main options
 
     ## Necessary arguments
-    parser.add_argument("--crab-config-patterns", nargs='+', required=True, help="List of path patterns to the crab configuration files, processed with glob")
-    parser.add_argument("--maxmemory", default=None, help="Maximum memory threshold in MB for resubmission passed to crab")
-    parser.add_argument("--maxjobruntime", default=None, help="Maximum job runtime threshold in minutes for resubmission passed to crab")
-    parser.add_argument("--nworkers", default=5, type=int, help="Number of workers to manage the crab tasks simultaneously")
-    parser.add_argument("--sitewhitelist",default=None,help="Comma-separated list of sites to exclusively run your jobs on.")
-    parser.add_argument("--siteblacklist",default=None,help="Comma-separated list of sites to avoid running your jobs there.")
+    parser.add_argument(
+        "--crab-config-patterns",
+        nargs="+",
+        required=True,
+        help="List of path patterns to the crab configuration files, processed with glob",
+    )
+    parser.add_argument(
+        "--maxmemory",
+        default=None,
+        help="Maximum memory threshold in MB for resubmission passed to crab",
+    )
+    parser.add_argument(
+        "--maxjobruntime",
+        default=None,
+        help="Maximum job runtime threshold in minutes for resubmission passed to crab",
+    )
+    parser.add_argument(
+        "--nworkers",
+        default=5,
+        type=int,
+        help="Number of workers to manage the crab tasks simultaneously",
+    )
+    parser.add_argument(
+        "--sitewhitelist",
+        default=None,
+        help="Comma-separated list of sites to exclusively run your jobs on.",
+    )
+    parser.add_argument(
+        "--siteblacklist",
+        default=None,
+        help="Comma-separated list of sites to avoid running your jobs there.",
+    )
 
     return parser.parse_args()
+
 
 def load_config(config_path):
     spec = importlib.util.spec_from_file_location("crab_config_module", config_path)
@@ -44,127 +67,190 @@ def load_config(config_path):
     spec.loader.exec_module(module)
     return module.config
 
+
 async def submit(config, logger, nworkers):
     loop = asyncio.get_event_loop()
     try:
-        sleep_duration = random.randint(0,12*nworkers)
+        sleep_duration = random.randint(0, 12 * nworkers)
         await asyncio.sleep(sleep_duration)
-        return await loop.run_in_executor(None, lambda: crabCommand('submit', config=config))
+        return await loop.run_in_executor(
+            None, lambda: crabCommand("submit", config=config)
+        )
     except Exception as e:
         logger.error(f"Failed submitting task:\n{e}")
-        cfg_directory = os.path.join(config.General.workArea, "crab_" + config.General.requestName)
+        cfg_directory = os.path.join(
+            config.General.workArea, "crab_" + config.General.requestName
+        )
         logger.debug(f"Cleaning up config directory {cfg_directory}")
         shutil.rmtree(cfg_directory)
         return None
 
+
 async def status(cfg_dir, logger, nworkers):
     loop = asyncio.get_event_loop()
     try:
-        sleep_duration = random.randint(0,12*nworkers)
+        sleep_duration = random.randint(0, 12 * nworkers)
         await asyncio.sleep(sleep_duration)
-        return await loop.run_in_executor(None, lambda: crabCommand('status', dir=cfg_dir))
+        return await loop.run_in_executor(
+            None, lambda: crabCommand("status", dir=cfg_dir)
+        )
     except Exception as e:
         logger.error(f"Failed querying status of task:\n{e}")
         return None
 
+
 async def resubmit(cfg_dir, logger, nworkers, **kwargs):
     loop = asyncio.get_event_loop()
     try:
-        sleep_duration = random.randint(0,12*nworkers)
+        sleep_duration = random.randint(0, 12 * nworkers)
         await asyncio.sleep(sleep_duration)
-        return await loop.run_in_executor(None, lambda: crabCommand('resubmit', dir=cfg_dir, **kwargs))
+        return await loop.run_in_executor(
+            None, lambda: crabCommand("resubmit", dir=cfg_dir, **kwargs)
+        )
     except Exception as e:
         logger.error(f"Failed resubmitting task:\n{e}")
         return None
 
+
 async def run_dasgoclient_query(dataset, instance):
-    command = ["dasgoclient", "-query", f"dataset dataset={dataset} instance={instance}", "-json"]
-    process = await asyncio.create_subprocess_exec(*command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    command = [
+        "dasgoclient",
+        "-query",
+        f"dataset dataset={dataset} instance={instance}",
+        "-json",
+    ]
+    process = await asyncio.create_subprocess_exec(
+        *command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
     stdout, stderr = await process.communicate()
     if process.returncode != 0:
         raise RuntimeError(f"Failed to query DAS: {stderr.decode().strip()}")
     return json.loads(stdout.decode().strip())
 
-async def worker(queue, args, worker_id, nworkers):
+
+async def worker(
+    submission_queue, status_queue, args, worker_id, nworkers, is_submission_worker
+):
     # Set up logging for this worker
     logs_directory = pathlib.Path("logs")
-    logs_directory.mkdir(mode=0o755,parents=True,exist_ok=True)
-    logs_path = logs_directory / f'worker_{worker_id}.log'
+    logs_directory.mkdir(mode=0o755, parents=True, exist_ok=True)
+    logs_path = logs_directory / f"worker_{worker_id}.log"
 
-    logger = logging.getLogger(f'worker_{worker_id}')
+    logger = logging.getLogger(f"worker_{worker_id}")
     logger.setLevel(logging.DEBUG)
     handler = logging.FileHandler(str(logs_path))
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
     while True:
+        queue = submission_queue if is_submission_worker else status_queue
         cfgpath = await queue.get()
         if cfgpath is None:
             break
 
         cfg = load_config(cfgpath)
-        cfg_directory = os.path.join(cfg.General.workArea, "crab_" + cfg.General.requestName)
+        cfg_directory = os.path.join(
+            cfg.General.workArea, "crab_" + cfg.General.requestName
+        )
         res = None
-        if not os.path.isdir(cfg_directory):
-            logger.info(f"Submitting {cfgpath}")
-            while not res:
-                res = await submit(cfg, logger, nworkers)
-                await asyncio.sleep(10)
-            await asyncio.sleep(600)
 
-        n_published = -1
-        n_finished = -2
-        n_all = -3
-        while n_all != n_finished or n_all != n_published:
-            logger.info(f"Checking task status for {cfg_directory}")
-            res = await status(cfg_directory, logger, nworkers)
-            if res:
-                #json.dump(res, open("res.json", "w"), indent=2, sort_keys=True)
-                n_all = sum([res["jobsPerStatus"][state] for state in res["jobsPerStatus"].keys()])
-                n_intermediate = res["jobsPerStatus"].get("idle", 0) + res["jobsPerStatus"].get("running", 0)
-                n_finished = res["jobsPerStatus"].get("finished", 0)
-                n_failed = res["jobsPerStatus"].get("failed", 0)
-                n_published = res["publication"].get("done", 0)
-                logger.info(f"Number of jobs: all = {n_all}, intermediate = {n_intermediate}, finished = {n_finished}, failed = {n_failed}, published = {n_published}")
-                kwargs = {k: v for k, v in {
-                    "maxmemory": args.maxmemory,
-                    "maxjobruntime": args.maxjobruntime,
-                    "sitewhitelist": args.sitewhitelist,
-                    "siteblacklist": args.siteblacklist,
-                }.items() if v is not None}
-                if n_intermediate == 0 and n_failed > 0 and kwargs:
-                    logger.info(f"Resubmitting task for {cfg_directory}")
-                    resub = None
-                    while not resub:
-                        resub = await resubmit(cfg_directory, logger, nworkers, **kwargs)
-                        await asyncio.sleep(10)
-            await asyncio.sleep(0 if n_all== n_finished and n_all == n_published else 900)
-        logger.info(f"Task {cfg_directory} finished. Output datasets:")
-        for dataset in ast.literal_eval(res["outdatasets"]):
-            try:
-                das_output = await run_dasgoclient_query(dataset, "prod/phys03")
-                das_input = await run_dasgoclient_query(cfg.Data.inputDataset, "prod/global")
-                nevents_output = None
-                nevents_input = None
-                for o in das_output:
-                    for do in o['dataset']:
-                        if "nevents" in do:
-                            nevents_output = do['nevents']
-                            break
-                for i in das_input:
-                    for di in i['dataset']:
-                        if "nevents" in di:
-                            nevents_input = di['nevents']
-                            break
-                if nevents_input is None or nevents_output is None or nevents_input != nevents_output:
-                    logger.error(f"Numbers of events in input {cfg.Data.inputDataset} ({nevents_input}) does not match output {dataset}: {nevents_output}. Crab task FAILED.")
-                else:
-                    logger.info(f"\t{dataset}: {nevents_output} events, consistent with input dataset")
-            except Exception as e:
-                logger.error(f"Failed to query DAS for dataset {dataset} and its parent:\n{e}")
+        if is_submission_worker:
+            if not os.path.isdir(cfg_directory):
+                logger.info(f"Submitting {cfgpath}")
+                res = await submit(cfg, logger, nworkers)
+            else:
+                logger.info(
+                    f"Task directory exists: {cfg_directory}. Considering as submitted."
+                )
+            # Enqueue the task for status checking
+            await status_queue.put(cfgpath)
+
+        else:
+            n_published = -1
+            n_finished = -2
+            n_all = -3
+            while n_all != n_finished or n_all != n_published:
+                logger.info(f"Checking task status for {cfg_directory}")
+                res = await status(cfg_directory, logger, nworkers)
+                if res:
+                    n_all = sum(
+                        [
+                            res["jobsPerStatus"][state]
+                            for state in res["jobsPerStatus"].keys()
+                        ]
+                    )
+                    n_intermediate = res["jobsPerStatus"].get("idle", 0) + res[
+                        "jobsPerStatus"
+                    ].get("running", 0)
+                    n_finished = res["jobsPerStatus"].get("finished", 0)
+                    n_failed = res["jobsPerStatus"].get("failed", 0)
+                    n_published = res["publication"].get("done", 0)
+                    logger.info(
+                        f"Number of jobs: all = {n_all}, intermediate = {n_intermediate}, finished = {n_finished}, failed = {n_failed}, published = {n_published}"
+                    )
+                    kwargs = {
+                        k: v
+                        for k, v in {
+                            "maxmemory": args.maxmemory,
+                            "maxjobruntime": args.maxjobruntime,
+                            "sitewhitelist": args.sitewhitelist,
+                            "siteblacklist": args.siteblacklist,
+                        }.items()
+                        if v is not None
+                    }
+                    if n_intermediate == 0 and n_failed > 0 and kwargs:
+                        logger.info(f"Resubmitting task for {cfg_directory}")
+                        resub = None
+                        while not resub:
+                            resub = await resubmit(
+                                cfg_directory, logger, nworkers, **kwargs
+                            )
+                            await asyncio.sleep(10)
+                await asyncio.sleep(
+                    0 if n_all == n_finished and n_all == n_published else 900
+                )
+            logger.info(f"Task {cfg_directory} finished. Output datasets:")
+            for dataset in ast.literal_eval(res["outdatasets"]):
+                try:
+                    das_output = await run_dasgoclient_query(dataset, "prod/phys03")
+                    das_input = await run_dasgoclient_query(
+                        cfg.Data.inputDataset, "prod/global"
+                    )
+                    nevents_output = None
+                    nevents_input = None
+                    for o in das_output:
+                        for do in o["dataset"]:
+                            if "nevents" in do:
+                                nevents_output = do["nevents"]
+                                break
+                    for i in das_input:
+                        for di in i["dataset"]:
+                            if "nevents" in di:
+                                nevents_input = di["nevents"]
+                                break
+                    if (
+                        nevents_input is None
+                        or nevents_output is None
+                        or nevents_input != nevents_output
+                    ):
+                        logger.error(
+                            f"Numbers of events in input {cfg.Data.inputDataset} ({nevents_input}) does not match output {dataset}: {nevents_output}. Crab task FAILED."
+                        )
+                    else:
+                        logger.info(
+                            f"\t{dataset}: {nevents_output} events, consistent with input dataset"
+                        )
+                except Exception as e:
+                    logger.error(
+                        f"Failed to query DAS for dataset {dataset} and its parent:\n{e}"
+                    )
+
         queue.task_done()
+
 
 async def main():
     args = parse_args()
@@ -174,23 +260,56 @@ async def main():
     for pattern in args.crab_config_patterns:
         config_paths.extend(glob.glob(pattern))
 
-    queue = asyncio.Queue()
+    submission_queue = asyncio.Queue()
+    status_queue = asyncio.Queue()
 
-    # Create worker tasks
+    # Create a single submission worker
+    submission_worker = asyncio.create_task(
+        worker(
+            submission_queue,
+            status_queue,
+            args,
+            "submission",
+            1,
+            is_submission_worker=True,
+        )
+    )
+
+    # Create multiple status workers
     nworkers = min(len(config_paths), args.nworkers)
-    workers = [asyncio.create_task(worker(queue, args, i, nworkers)) for i in range(nworkers)]
+    status_workers = [
+        asyncio.create_task(
+            worker(
+                submission_queue,
+                status_queue,
+                args,
+                i,
+                nworkers,
+                is_submission_worker=False,
+            )
+        )
+        for i in range(nworkers)
+    ]
 
-    # Enqueue config paths
+    # Enqueue config paths for submission
     for cfgpath in config_paths:
-        await queue.put(cfgpath)
+        await submission_queue.put(cfgpath)
 
-    # Wait until the queue is fully processed
-    await queue.join()
+    # Wait until the submission queue is fully processed
+    await submission_queue.join()
+
+    # Stop the submission worker:
+    await submission_queue.put(None)
+    await submission_worker
+
+    # Wait until the status queue is fully processed
+    await status_queue.join()
 
     # Stop workers
     for _ in range(nworkers):
-        await queue.put(None)
-    await asyncio.gather(*workers)
+        await status_queue.put(None)
+    await asyncio.gather(*status_workers)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- create_configs.py
+++ create_configs.py
@@ -14,40 +14,104 @@ from crab_configuration.crab_template import config
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Script to prepare a CMSSW production with crab",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
     # Main options
 
     ## Necessary arguments
-    parser.add_argument("--work-directory",required=True,help="Path to the main work directory")
-    parser.add_argument("--datasets",required=True,help="Path to the dataset configuration .yaml file")
-    
+    parser.add_argument(
+        "--work-directory", required=True, help="Path to the main work directory"
+    )
+    parser.add_argument(
+        "--datasets", required=True, help="Path to the dataset configuration .yaml file"
+    )
+
     ## Optional arguments
 
     # CMSSW configuration group
-    cmssw = parser.add_argument_group("CMSSW", "Configuration related to CMSSW settings to run a cmsRun config")
+    cmssw = parser.add_argument_group(
+        "CMSSW", "Configuration related to CMSSW settings to run a cmsRun config"
+    )
 
     ## Necessary arguments
-    cmssw.add_argument("--conditions",required=True,help="Path to the conditions configuration .yaml file")
-    cmssw.add_argument("--cmsdriver",required=True,help="Path to the cmsdriver configuration .yaml file")
-    cmssw.add_argument("--nThreads",type=int,default=1,help="Number of threads to be used for the cmsRun config")
-    cmssw.add_argument("--nStreams",type=int,default=0,help="Number of streams to be used for the cmsRun config. If set to 0, nThreads will be taken by cmsRun.")
+    cmssw.add_argument(
+        "--conditions",
+        required=True,
+        help="Path to the conditions configuration .yaml file",
+    )
+    cmssw.add_argument(
+        "--cmsdriver",
+        required=True,
+        help="Path to the cmsdriver configuration .yaml file",
+    )
+    cmssw.add_argument(
+        "--nThreads",
+        type=int,
+        default=1,
+        help="Number of threads to be used for the cmsRun config",
+    )
+    cmssw.add_argument(
+        "--nStreams",
+        type=int,
+        default=0,
+        help="Number of streams to be used for the cmsRun config. If set to 0, nThreads will be taken by cmsRun.",
+    )
 
     # Crab configuration group
-    crab = parser.add_argument_group("crab3", "Configuration related to crab3 settings to submit a crab task")
+    crab = parser.add_argument_group(
+        "crab3", "Configuration related to crab3 settings to submit a crab task"
+    )
 
     ## Optional arguments
-    crab.add_argument("--numCores",type=int,default=1,help="Number of Cores used by crab for the job")
-    crab.add_argument("--maxMemoryMBperCore",type=int,default=2000,help="Maximum memory in MB to be used by the crab job per core")
-    crab.add_argument("--maxJobRuntimeMin",type=int,default=720,help="Maximum runtime expected for the job im minutes")
-    crab.add_argument("--publication",action="store_true",help="Flag to decide, whether to publish crab output dataset")
-    crab.add_argument("--splitting",choices=["Automatic", "FileBased", "LumiBased", "EventAwareLumiBased"],default="Automatic",help="Choice of splitting of the task into jobs.")
-    crab.add_argument("--unitsPerJob",type=int,default=720,help="Rough number of files (FileBased), luminosity sections (LumiBased), or events (EventAwareLumiBased) to have per job, depending on the corresponding splitting. In case of Automatic splitting: target runtime in minutes with a minimum of 180.")
-    crab.add_argument("--siteWhitelist",nargs='*',help="List of sites to exclusively run your jobs on.")
-    crab.add_argument("--siteBlacklist",nargs='*',help="List of sites to avoid running your jobs there.")
+    crab.add_argument(
+        "--numCores",
+        type=int,
+        default=1,
+        help="Number of Cores used by crab for the job",
+    )
+    crab.add_argument(
+        "--maxMemoryMBperCore",
+        type=int,
+        default=2000,
+        help="Maximum memory in MB to be used by the crab job per core",
+    )
+    crab.add_argument(
+        "--maxJobRuntimeMin",
+        type=int,
+        default=720,
+        help="Maximum runtime expected for the job im minutes",
+    )
+    crab.add_argument(
+        "--publication",
+        action="store_true",
+        help="Flag to decide, whether to publish crab output dataset",
+    )
+    crab.add_argument(
+        "--splitting",
+        choices=["Automatic", "FileBased", "LumiBased", "EventAwareLumiBased"],
+        default="Automatic",
+        help="Choice of splitting of the task into jobs.",
+    )
+    crab.add_argument(
+        "--unitsPerJob",
+        type=int,
+        default=720,
+        help="Rough number of files (FileBased), luminosity sections (LumiBased), or events (EventAwareLumiBased) to have per job, depending on the corresponding splitting. In case of Automatic splitting: target runtime in minutes with a minimum of 180.",
+    )
+    crab.add_argument(
+        "--siteWhitelist",
+        nargs="*",
+        help="List of sites to exclusively run your jobs on.",
+    )
+    crab.add_argument(
+        "--siteBlacklist",
+        nargs="*",
+        help="List of sites to avoid running your jobs there.",
+    )
 
     return parser.parse_args()
+
 
 def initialize(args):
     args_dict = vars(args)
@@ -58,10 +122,11 @@ def initialize(args):
     args_dict["timestamp"] = str(int(datetime.datetime.now().timestamp()))
     return args_dict
 
+
 def prepare(args):
     # Ensure, that work directory exists
     work_directory = pathlib.Path(args["work_directory"])
-    work_directory.mkdir(mode=0o755,parents=True,exist_ok=True)
+    work_directory.mkdir(mode=0o755, parents=True, exist_ok=True)
 
     # Prepare cmsDriver commands and create cmsRun configs
     cmsdriver_directory = work_directory / "cmsdriver"
@@ -70,7 +135,9 @@ def prepare(args):
     cmsdriver_keys = set(args["cmsdriver"].keys())
     conditions_keys = set(args["conditions"].keys())
     if cmsdriver_keys != conditions_keys:
-        print(f"Error: expecting keys for cmsdriver keys {cmsdriver_keys} to be the same as condition keys {conditions_keys}. This is not the case")
+        print(
+            f"Error: expecting keys for cmsdriver keys {cmsdriver_keys} to be the same as condition keys {conditions_keys}. This is not the case"
+        )
         exit(1)
 
     cmsdriver_commands = []
@@ -83,14 +150,16 @@ def prepare(args):
             command = f'cmsDriver.py {cmsdriverspecs["type"]} --{k} --filein file:{cmsdriverspecs["filein"]} --fileout {cmsdriverspecs["fileout"]} --step {cmsdriverspecs["step"]} --eventcontent {cmsdriverspecs["eventcontent"]} --datatier {cmsdriverspecs["datatier"]} --python_filename {str(cmsrun)} --conditions {configuration["globaltag"]} --era {configuration["era"]} --no_exec --nThreads {args["nThreads"]} --nStreams {args["nStreams"]}'
             print(f"Running: {command}")
             # TODO: introduce a checksum check to be sure, that the new round creates the same config, if one exists.
-            result = subprocess.run(shlex.split(command), capture_output=True, text=True)
+            result = subprocess.run(
+                shlex.split(command), capture_output=True, text=True
+            )
             print(f"\tReturn code: {result.returncode}")
 
     # Construct general crab config based on provided crab3 settings
     crabworkarea = work_directory / "crab"
-    crabworkarea.mkdir(mode=0o755,parents=True,exist_ok=True)
+    crabworkarea.mkdir(mode=0o755, parents=True, exist_ok=True)
     crabconfigarea = work_directory / "crabconfigs"
-    crabconfigarea.mkdir(mode=0o755,parents=True,exist_ok=True)
+    crabconfigarea.mkdir(mode=0o755, parents=True, exist_ok=True)
     args["crab"].General.workArea = str(crabworkarea)
     args["crab"].JobType.numCores = args["numCores"]
     args["crab"].JobType.maxMemoryMB = args["maxMemoryMBperCore"] * args["numCores"]
@@ -105,7 +174,7 @@ def prepare(args):
 
     args["crabconfigs"] = {}
     # Loop through the datasets and create dataset-specific crab configuration files
-    for k, dt_period_config in args["datasets"].items(): # k is key from {data, mc}
+    for k, dt_period_config in args["datasets"].items():  # k is key from {data, mc}
         args["crabconfigs"][k] = {}
         for dt_period, dataset_type_config in dt_period_config.items():
             args["crabconfigs"][k][dt_period] = {}
@@ -113,19 +182,34 @@ def prepare(args):
                 args["crabconfigs"][k][dt_period][dataset_type] = {}
                 for dkey, dname in datasets.items():
                     dataset_crab = copy.deepcopy((args["crab"]))
-                    dataset_crab.General.requestName = "_".join([k,dt_period,dataset_type,dkey])
-                    dataset_crab.JobType.psetName = str(args["conditions"][k][dt_period]["cmsrun"])
+                    dataset_crab.General.requestName = "_".join(
+                        [k, dt_period, dataset_type, dkey]
+                    )
+                    dataset_crab.JobType.psetName = str(
+                        args["conditions"][k][dt_period]["cmsrun"]
+                    )
                     dataset_crab.Data.inputDataset = dname
-                    dataset_crab.Data.outputDatasetTag = dataset_crab.General.requestName + "_" + args["timestamp"]
-                    dataset_crab_path = crabconfigarea / dataset_crab.General.requestName
-                    args["crabconfigs"][k][dt_period][dataset_type][dkey] = str(dataset_crab_path) + ".py"
+                    dataset_crab.Data.outputDatasetTag = (
+                        dataset_crab.General.requestName + "_" + args["timestamp"]
+                    )
+                    dataset_crab_path = (
+                        crabconfigarea / dataset_crab.General.requestName
+                    )
+                    args["crabconfigs"][k][dt_period][dataset_type][dkey] = (
+                        str(dataset_crab_path) + ".py"
+                    )
                     # TODO: introduce a check for existence and checksum check
-                    print(f'Creating crab config: {args["crabconfigs"][k][dt_period][dataset_type][dkey]}')
-                    with open(args["crabconfigs"][k][dt_period][dataset_type][dkey], "w") as out_crab:
+                    print(
+                        f'Creating crab config: {args["crabconfigs"][k][dt_period][dataset_type][dkey]}'
+                    )
+                    with open(
+                        args["crabconfigs"][k][dt_period][dataset_type][dkey], "w"
+                    ) as out_crab:
                         out_crab.write(str(dataset_crab))
 
     return args
-                    
+
+
 if __name__ == "__main__":
     args = initialize(parse_args())
     args = prepare(args)
